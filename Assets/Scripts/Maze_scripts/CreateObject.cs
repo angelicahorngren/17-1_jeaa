ff20f242ba554c0b0e8aad16356d6d31afae4bd2
Adam Boman <adamoboman@gmail.com>
2023-02-22 15:44:26 +0100

    Labyrint_fixes

---
+++ Assets/Scripts/Maze_scripts/CreateObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObject : MonoBehaviour
+{
+    public Transform Spawnpoint;
+    public GameObject Prefab;
+    private bool hasSpawned = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!hasSpawned)
+        {
+            Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation);
+            hasSpawned = true;
+        }
+    }
+}
