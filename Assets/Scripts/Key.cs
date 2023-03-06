4c7095e8963217df52b74e72be93e067f5a0a95a
angelicahorngren <94979102+angelicahorngren@users.noreply.github.com>
2023-03-06 22:01:52 +0100

    changes

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -15,13 +15,11 @@ public class Key : MonoBehaviour
         }
     }
 
-    // Returns true if the player is looking at the key
+
     bool IsPlayerLookingAtKey()
     {
-        // Get the direction from the player to the key
+ 
         Vector3 direction = transform.position - Camera.main.transform.position;
-
-        // If the angle between the camera's forward direction and the direction to the key is less than 45 degrees, the player is looking at the key
         return Vector3.Angle(Camera.main.transform.forward, direction) < 45f;
     }
 }
